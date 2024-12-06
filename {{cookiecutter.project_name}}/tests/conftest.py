86aab0142d385fb20548e0b3bbee684eba070491
Ivo Bellin Salarin <ivo@nilleb.com>
2024-12-06 16:25:36 +0100

    feat: router tests

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -1,10 +1,15 @@
 from typing import AsyncGenerator
-
+import logging
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from sqlmodel import SQLModel
+from httpx import AsyncClient
+from unittest.mock import patch
+from api.logger import logger
 
 from api.database.sql import engine as async_engine
+from api.app import app
+
 
 @pytest_asyncio.fixture(scope="function")
 async def async_session() -> AsyncGenerator:
@@ -23,3 +28,20 @@ async def async_session() -> AsyncGenerator:
 
     await async_engine.dispose()
     await async_engine.dispose()
+
+
+@pytest_asyncio.fixture()
+async def async_client():
+    headers = {"Authorization": "Bearer test_token"}
+    async with AsyncClient(
+        app=app,
+        base_url="http://localhost:17581",
+        headers=headers,
+    ) as client:
+        yield client
+
+
+@pytest_asyncio.fixture()
+def standard_logging():
+    with patch.object(logger, "logger", return_value=logging):
+        yield
