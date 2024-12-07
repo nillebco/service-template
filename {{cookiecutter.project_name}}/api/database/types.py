5818663338d3a3ae5f76b64e84a9f8e0aa00c208
Ivo Bellin Salarin <ivo@nilleb.com>
2024-12-07 07:44:09 +0100

    fix: dynamic media uid attribute

--- {{cookiecutter.project_name}}/api/database/types.py
+++ {{cookiecutter.project_name}}/api/database/types.py
@@ -10,7 +10,7 @@ class DynamicMedia(SQLModel, table=True):
     Associates an ID to a media.
     """
 
-    uid: Optional[int] = Field(default=None, primary_key=True)
+    uid: Optional[str] = Field(default=None, primary_key=True)
     kind: str
     entity_id: str
     created_at: Optional[float] = Field(default_factory=lambda: utc_now_float())
