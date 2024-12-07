cdf8f96d98b1a6e91e0529b7234855918bef9101
Ivo Bellin Salarin <ivo@nilleb.com>
2024-12-07 07:49:30 +0100

    chore: const types

--- {{cookiecutter.project_name}}/api/constants.py
+++ {{cookiecutter.project_name}}/api/constants.py
@@ -4,9 +4,9 @@ import os
 AUTHOR = "{{cookiecutter.full_name}} <{{cookiecutter.email}}>"
 APP_NAME = "{{cookiecutter.project_name}}"
 
-IS_TESTING = "pytest" in sys.modules
-IS_DEV = os.getenv("IS_DEV")
-DATA_FOLDER = "data"
-API_PREFIX = "/api/v1"
+IS_TESTING : bool = "pytest" in sys.modules
+IS_DEV : bool = os.getenv("IS_DEV") == "true"
+DATA_FOLDER : str = "data"
+API_PREFIX : str = "/api/v1"
 
-MEDIA_DYNAMIC_API = "/media/dynamic"
+MEDIA_DYNAMIC_API : str = "/media/dynamic"
