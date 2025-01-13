939139d7b0b3294445b43651b82c1c38c5c727ae
Ivo Bellin Salarin <ivo@nilleb.com>
2025-01-13 20:56:04 +0100

    fix: cookiecutter template for create database sql

--- {{cookiecutter.project_name}}/create-database.sql
+++ {{cookiecutter.project_name}}/create-database.sql
@@ -1,8 +1,8 @@
 -- execute these lines one at a time, being connected as the "main" user of the database
-create database "{{project_name}}";
-create user "{{project_name}}-user" with encrypted password '{{ random_ascii_string(12) }}';
-grant all privileges on database "{{project_name}}" to "{{project_name}}-user";
+create database "{{cookiecutter.project_name}}";
+create user "{{cookiecutter.project_name}}-user" with encrypted password '{{ random_ascii_string(12) }}';
+grant all privileges on database "{{cookiecutter.project_name}}" to "{{cookiecutter.project_name}}-user";
 -- change database connection
-\c {{project_name}}
-GRANT ALL ON SCHEMA public TO "{{project_name}}-user";
-GRANT USAGE ON SCHEMA public TO "{{project_name}}-user";
+\c {{cookiecutter.project_name}}
+GRANT ALL ON SCHEMA public TO "{{cookiecutter.project_name}}-user";
+GRANT USAGE ON SCHEMA public TO "{{cookiecutter.project_name}}-user";
