c4c3e753e0e69b10f3395de2ed712587ad8a2a67
Ivo Bellin Salarin <ivo@nilleb.com>
2025-12-15 16:03:47 +0100

    fix: don't detect changes on random values

--- {{cookiecutter.project_name}}/create-database.sql
+++ {{cookiecutter.project_name}}/create-database.sql
@@ -1,6 +1,6 @@
 -- execute these lines one at a time, being connected as the "main" user of the database
 create database "{{cookiecutter.project_name}}";
-create user "{{cookiecutter.project_name}}-user" with encrypted password '{{ random_ascii_string(12) }}';
+create user "{{cookiecutter.project_name}}-user" with encrypted password '{{ cookiecutter.db_password }}';
 grant all privileges on database "{{cookiecutter.project_name}}" to "{{cookiecutter.project_name}}-user";
 -- change database connection
 \c {{cookiecutter.project_name}}
